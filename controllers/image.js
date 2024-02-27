b624b6169e88e845bd3711c78b123838ca6c10b7
Rui Custódio <ruifscustodio@gmail.com>
2024-02-27 18:36:36 +0000

    first commit

---
+++ controllers/image.js
@@ -0,0 +1,99 @@
+const PAT = '6b418b053770411e8c9ed931dddfc3e0';
+const USER_ID = 'valyndis';
+const APP_ID = 'my-first-application';
+const MODEL_ID = 'face-detection';
+const { ClarifaiStub, grpc } = require("clarifai-nodejs-grpc");
+
+const stub = ClarifaiStub.grpc();
+
+const metadata = new grpc.Metadata();
+metadata.set("authorization", "Key " + PAT);
+
+const handleApiCall = (req, res) => {
+    const IMAGE_URL = req.body.input;
+    stub.PostModelOutputs(
+        {
+            user_app_id: {
+                "user_id": USER_ID,
+                "app_id": APP_ID
+            },
+            model_id: MODEL_ID,
+            inputs: [
+                { data: { image: { url: IMAGE_URL, allow_duplicate_url: true } } }
+            ]
+        },
+        metadata,
+        (err, response) => {
+            const output = response.outputs[0];
+
+            console.log("Predicted concepts:");
+            for (const concept of output.data.concepts) {
+                console.log(concept.name + " " + concept.value);
+            }
+            res.json(output)
+        }
+    )
+}
+
+
+
+
+
+
+/* const returnClarifaiRequestOptions = (imageUrl) => {
+    const PAT = '6b418b053770411e8c9ed931dddfc3e0';
+    const USER_ID = 'valyndis';       
+    const APP_ID = 'my-first-application';
+    const MODEL_ID = 'face-detection';  
+    const IMAGE_URL = imageUrl;
+    const raw = JSON.stringify({
+      "user_app_id": {
+          "user_id": USER_ID,
+          "app_id": APP_ID
+      },
+      "inputs": [
+          {
+              "data": {
+                  "image": {
+                      "url": IMAGE_URL
+                  }
+              }
+          }
+      ]
+  });
+  
+  const requestOptions = {
+      method: 'POST',
+      headers: {
+          'Accept': 'application/json',
+          'Authorization': 'Key ' + PAT
+      },
+      body: raw
+  };
+  return requestOptions
+}
+
+const handleApiCall = (req, res) => {
+    fetch("https://api.clarifai.com/v2/models/" + 'face-detection' + "/outputs", returnClarifaiRequestOptions(req.body.input))
+    .then(data => {
+        res.json(data);
+    })
+    .catch(err => res.status(400).json('unable to communicate with clarifai API'))
+} */
+
+
+const handleImage = (req, res, db) => {
+    const {id} = req.body;
+    db('users').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+        res.json(entries[0].entries);
+    })
+    .catch(err => res.status(400).json('unable to increment entries'))
+}
+
+module.exports = {
+    handleImage,
+    handleApiCall
+}
