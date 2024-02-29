518608c9a4dfef348eb2d77a90dca8e4a5b259c3
Rui Custódio <ruifscustodio@gmail.com>
2024-02-29 17:42:45 +0000

    env variable working

--- server.js
+++ server.js
@@ -36,5 +36,4 @@ app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) });
 
 app.listen(process.env.PORT || 3000, () => {
     console.log(`app is running on port ${process.env.PORT}`);
-    console.log('env variable check', process.env.DATABASE_USER);
 });
